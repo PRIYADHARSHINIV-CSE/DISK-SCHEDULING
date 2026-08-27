76983e5f5fff589c7599e59cfe08147e9b81a11d
PRIYADHARSHINIV-CSE <priyadharshiniv.cse2025@citchennai.net>
2026-08-27 13:34:03 +0530

    Add SCAN disk scheduling script
    Implement SCAN disk scheduling algorithm to calculate total head movement based on a request queue.

---
+++ scan.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+queue=(98 183 37 122 14 124 65 67)
+head=53
+disk_size=200
+seek=0
+
+echo "SCAN Disk Scheduling"
+echo "Request Queue : ${queue[*]}"
+echo "Initial Head Position : $head"
+echo "Disk Size : $disk_size"
+
+# Sort request queue
+IFS=$'\n' sorted=($(sort -n <<<"${queue[*]}"))
+unset IFS
+
+# Move towards higher cylinders
+for req in "${sorted[@]}"
+do
+    if [ $req -ge $head ]
+    then
+        diff=$((req - head))
+        seek=$((seek + diff))
+        head=$req
+    fi
+done
+
+# Move to the end
+seek=$((seek + (disk_size - 1 - head)))
+head=$((disk_size - 1))
+
+# Reverse direction
+for ((i=${#sorted[@]}-1; i>=0; i--))
+do
+    req=${sorted[i]}
+
+    if [ $req -lt $head ]
+    then
+        diff=$((head - req))
+        seek=$((seek + diff))
+        head=$req
+    fi
+done
+
+echo "Total Head Movement = $seek"
